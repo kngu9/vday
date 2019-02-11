4300c776bef8f9e3e30ead595d80ecef00a3c3cb
Jessica Tjahja <agnes.j.tjahja@gmail.com>
2019-02-11 03:06:30 -0600

    final maybe

--- react-client/src/components/headerComponent/jumbotron.js
+++ react-client/src/components/headerComponent/jumbotron.js
@@ -5,6 +5,7 @@ class Jumbotron extends Component{
         return(
             <div class="jumbotron">
                 <h1 class="display-4">Happy Valentine's Day</h1>
+                <h2 class="lead">These are some of our fun times together.</h2>
             </div>
         )
     }
--- react-client/src/components/photos.js
+++ react-client/src/components/photos.js
@@ -3,15 +3,49 @@ import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 
 const photos = [
-  { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799', width: 1, height: 1 },
-  { src: 'https://source.unsplash.com/qDkso9nvCg0/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/iecJiKe_RNg/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/zh7GEuORbUw/600x799', width: 3, height: 4 },
-  { src: 'https://source.unsplash.com/PpOHJezOalU/800x599', width: 4, height: 3 },
-  { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
+  { src: 'https://i.ibb.co/CwpJpcC/20181104-213133.jpg', width: 4, height: 3 },
+  { src: 'https://i.ibb.co/VmxjV0p/20181109-224628.jpg', width: 2, height: 4 },
+  { src: 'https://i.ibb.co/hdBkyYD/20181202-021633.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/bj9t4mk/20181216-174559.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/ftdHBhc/IMG-0011.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/bPx5kS6/IMG-0024.jpg', width: 4, height: 3 },
+
+  { src: 'https://i.ibb.co/4tCZFWt/IMG-0031.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/zFW4dk1/IMG-0039.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/bdcHS1x/IMG-0041.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/5xrr7c1/IMG-0077.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/vv0hWpd/IMG-0079.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/ygKnKsJ/IMG-0265.jpg', width: 4, height: 3 },
+
+  { src: 'https://i.ibb.co/8gG6Pq4/IMG-0266.jpg', width: 2, height: 4 },
+  { src: 'https://i.ibb.co/RNXzFWz/IMG-0270.jpg', width: 4, height: 4 },
+  { src: 'https://i.ibb.co/sqjrPNM/IMG-20181121-WA0004.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/CmvP3cP/IMG-20181125-WA0000.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/D45KL5g/IMG-20181126-WA0008.jpg', width: 2, height: 4 },
+  { src: 'https://i.ibb.co/kgcx1y5/IMG-20181128-WA0000.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/xf13yFf/IMG-20181214-WA0002.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/2ctwZjQ/IMG-20181216-WA0004.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/R6wKKhK/IMG-20181216-WA0006.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/XD6mz6D/received-364189511010051.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/X76mmxb/received-1130011930491157.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/vq8HJZt/received-2174945079185180.jpg', width: 4, height: 4 },
+  
+  { src: 'https://i.ibb.co/jVB5nn8/received-2971766319515519.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/KFcs6jV/Snapchat-1697520003.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/8XHDJjC/IMG-2493.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/7SbQ0cs/IMG-2821.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/4F6hS7j/IMG-5725.jpg', width: 1, height: 1 },
+  { src: 'https://i.ibb.co/FqhZ8Bn/IMG-7208.jpg', width: 3, height: 4 },
+
+  { src: 'https://i.ibb.co/rFr9jpC/IMG-3908.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/FnxyH43/IMG-8013.jpg', width: 3, height: 4 },
+  { src: 'https://i.ibb.co/VvZc6Ct/IMG-0784.jpg', width: 3, height: 4 }
 ];
 
 class Photos extends Component {
