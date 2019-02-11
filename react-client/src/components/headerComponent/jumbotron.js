04b93cd95fc8e977307abce54b527e02ee5b8165
Jessica Tjahja <agnes.j.tjahja@gmail.com>
2019-02-11 04:17:21 -0600

    made the text bigger

--- react-client/src/components/headerComponent/jumbotron.js
+++ react-client/src/components/headerComponent/jumbotron.js
@@ -3,9 +3,11 @@ import React, {Component} from 'react';
 class Jumbotron extends Component{
     render(){
         return(
-            <div class="jumbotron">
-                <h1 class="display-4">Happy Valentine's Day</h1>
-                <h2 class="lead">These are some of our fun times together.</h2>
+            <div class="container">
+                <div class="jumbotron">
+                    <h1 class="display-4">Happy Valentine's Day</h1>
+                    <h2 class="lead">These are some of our fun times together.</h2>
+                </div>
             </div>
         )
     }
